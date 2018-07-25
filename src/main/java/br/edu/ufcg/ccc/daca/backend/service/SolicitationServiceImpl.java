69deac9a0dac4d5528aa8f206d918b8960d34c23
Emanuel Joivo <emanueljoivo@gmail.com>
2018-07-25 01:31:54 -0300

    Solicitation repository

--- src/main/java/br/edu/ufcg/ccc/daca/backend/service/SolicitationServiceImpl.java
+++ src/main/java/br/edu/ufcg/ccc/daca/backend/service/SolicitationServiceImpl.java
@@ -1,7 +1,16 @@
 package br.edu.ufcg.ccc.daca.backend.service;
 
+import br.edu.ufcg.ccc.daca.backend.repository.SolicitationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SolicitationServiceImpl implements SolicitationService {
+
+    private SolicitationRepository solicitationRepository;
+
+    @Autowired
+    public SolicitationServiceImpl(SolicitationRepository solicitationRepository) {
+        this.solicitationRepository = solicitationRepository;
+    }
 }
