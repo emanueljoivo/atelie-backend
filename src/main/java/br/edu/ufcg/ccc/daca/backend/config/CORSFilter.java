b21a4d98849695cfc5d9e5f0a771295973885ddb
Emanuel Joivo Bezerra Martins <emanueljoivo@gmail.com>
2018-08-07 15:27:54 -0300

    Fix cors filter

--- src/main/java/br/edu/ufcg/ccc/daca/backend/config/CORSFilter.java
+++ src/main/java/br/edu/ufcg/ccc/daca/backend/config/CORSFilter.java
@@ -1,6 +1,8 @@
 package br.edu.ufcg.ccc.daca.backend.config;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,31 +14,43 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSFilter implements Filter {
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:4200");
 
-        HttpServletResponse response = (HttpServletResponse) res;
-        HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
-        if (request.getMethod().equals(HttpMethod.OPTIONS.name())){
-            response.setStatus(HttpStatus.NO_CONTENT.value());
-        } else{
-            chain.doFilter(req, res);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        // Lets make sure that we are working with HTTP (that is, against HttpServletRequest and HttpServletResponse objects)
+        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+            HttpServletRequest request = (HttpServletRequest) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+
+            // Access-Control-Allow-Origin
+            String origin = request.getHeader("Origin");
+            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.setHeader("Vary", "Origin");
+
+            // Access-Control-Max-Age
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            // Access-Control-Allow-Credentials
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+
+            // Access-Control-Allow-Methods
+            response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+
+            // Access-Control-Allow-Headers
+            response.setHeader("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
         }
+
+        chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) { }
 
     public void destroy() {}
 }
