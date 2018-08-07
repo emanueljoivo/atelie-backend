d7bc35c2ccc34c8196c31ba866ee27b9c4d17623
Emanuel Joivo Bezerra Martins <emanueljoivo@gmail.com>
2018-08-07 14:11:29 -0300

    change project structure

--- src/main/java/br/edu/ufcg/ccc/daca/backend/BackendApplication.java
+++ src/main/java/br/edu/ufcg/ccc/daca/backend/BackendApplication.java
@@ -2,7 +2,11 @@ package br.edu.ufcg.ccc.daca.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -10,5 +14,19 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                        .allowedOrigins(
+								"http://192.168.1.108:8080/**",
+                                " https://assueria-b46eb.firebaseapp.com/**"
+                        );
+			}
+		};
 	}
 }
--- src/main/java/br/edu/ufcg/ccc/daca/backend/config/WebMvcConfig.java
+++ src/main/java/br/edu/ufcg/ccc/daca/backend/config/WebMvcConfig.java
@@ -1,19 +1,2 @@
 package br.edu.ufcg.ccc.daca.backend.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8080",
-                        "https://assueria.firebaseapp.com",
-                        "https://assueria-b46eb.firebaseapp.com"
-                );
-    }
-}
