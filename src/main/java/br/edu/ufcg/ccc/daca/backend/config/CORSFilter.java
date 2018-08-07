be160ece1f71e24ef612b67f0dddaaa6115bb54e
Emanuel Joivo Bezerra Martins <emanueljoivo@gmail.com>
2018-08-07 15:38:16 -0300

    Adding authorization header

--- src/main/java/br/edu/ufcg/ccc/daca/backend/config/CORSFilter.java
+++ src/main/java/br/edu/ufcg/ccc/daca/backend/config/CORSFilter.java
@@ -47,7 +47,7 @@ public class CORSFilter implements Filter {
 
             // Access-Control-Allow-Headers
             response.setHeader("Access-Control-Allow-Headers",
-                    "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
+                    "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN" + "Authorization");
         }
 
         chain.doFilter(req, res);
